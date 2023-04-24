2b7f96866fcfe7e1f85fbe886fca01f5163d2a77
ispipa <ispipa123@gmail.com>
2023-04-24 16:18:13 +0200

    Screens

--- 02-MiPrimeraApp/.eslintrc.js
+++ 02-MiPrimeraApp/.eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+  //esto es para desactivar el prettier
+  rules: {
+    'prettier/prettier': 0,
+  },
   root: true,
   extends: '@react-native-community',
 };
---
+++ 02-MiPrimeraApp/src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,17 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export const HolaMundoScreen = () => {
+  return (
+      <View style={{
+          flex: 1,
+          justifyContent: 'center',
+      }}>
+          <Text style={{
+              fontSize: 45,
+              textAlign: 'center',
+          }}>Hola Mundo</Text>
+      </View>
+  );
+};
+
